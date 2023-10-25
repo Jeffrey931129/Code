d35f197b931607ebf1498632aa55991693bc7281
Jeffrey <168chen2011@gmail.com>
2023-10-26 07:57:37 +0800

    update

--- class/HW5/Fibonacci.c
+++ class/HW5/Fibonacci.c
@@ -6,25 +6,83 @@ int gib(int);
 int main()
 {
     scanf("%d%d%d%d%d",&f[0],&f[1],&g[0],&g[1],&n);
-    
+    for(int i=2;i<=n;i++)
+    {
+        f[i]=fib(i);
+    }
     printf("%d %d\n",fib(n),gib(n));
+    
     return 0;
 }
 
 int fib(int n)
 {
-    if(n=1) return f[1];
-    if(n=0) return f[0];
+    if(n==1) return f[1];
+    if(n==0) return f[0];
     if(f[n]!=0) return f[n];
-    if(n%3==0) return fib(n-1)+gib(n/3);
-    else return fib(n-1)+gib(n-2);
+    if(n%3==0) 
+    {
+        if(f[n-1]==0)
+        {
+            f[n-1]=fib(n-1);
+        }
+        if(f[n/3]==0)
+        {
+            f[n/3]=fib(n/3);
+        }
+        if(g[f[n/3]]==0)
+        {
+            g[f[n/3]]=gib(f[n/3]);
+        }
+        return f[n-1]+g[f[n/3]];
+    }
+    else 
+    {
+        if(f[n-1]==0)
+        {
+            f[n-1]=fib(n-1);
+        }
+        if(g[n-2]==0)
+        {
+            g[n-2]=gib(n-2);
+        }
+        return f[n-1]+g[n-2];
+    }
+    
 }
 
 int gib(int n)
 {
-    if(n=1) return g[1];
-    if(n=0) return g[0];
+    if(n==1) return g[1];
+    if(n==0) return g[0];
     if(g[n]!=0) return g[n];
-    if(n%5==0) return gib(n-1)+fib(n/5);
-    else return gib(n-1)+fib(n-2);
+    if(n%5==0)
+    {
+        if(g[n-1]==0)
+        {
+            g[n-1]=gib(n-1);
+        }
+        if(g[n/5]==0)
+        {
+            g[n/5]=gib(n/5);
+        }
+        if(f[g[n/5]]==0)
+        {
+            f[g[n/5]]=fib(g[n/5]);
+        }
+        return g[n-1]+f[g[n/5]];
+    }
+    else 
+    {
+        if(g[n-1]==0)
+        {
+            g[n-1]=gib(n-1);
+        }
+        if(f[n-2]==0)
+        {
+            f[n-2]=fib(n-2);
+        }
+        return g[n-1]+f[n-2];
+    }
+    
 }
--- self learn/0-test.c
+++ self learn/0-test.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 
-
+int tes(int);
 int main()
 {
     int b=10;
     
-    printf("%d\n1234567890123456789",--b);
+    printf("%d\n1234567890123456789",tes(5));
     return 0;
 }
-
+int tes(int n)
+{
+    return n;
+}
