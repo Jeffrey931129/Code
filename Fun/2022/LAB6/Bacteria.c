52164467f6aefdda6d81daf52851f96d705e7a1f
Jeffrey <168chen2011@gmail.com>
2023-10-31 11:26:05 +0800

    update

---
+++ Fun/2022/LAB6/Bacteria.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+int find[10000][2];
+int main()
+{
+    int r,c,t,i=0;
+    scanf("%d%d%d",&r,&c,&t);
+    char cl[r][c+1];
+    for(int x=0;x<r;x++)
+    {
+        scanf("%s",cl[x]);
+        for(int y=0;y<c+1;y++)
+        {
+            
+            if(cl[x][y]=='F')
+            {
+                find[i][0]=x;
+                find[i][1]=y;
+                i++;
+            }
+        }
+    }
+    
+    int j=0;
+    while(t--)
+    {
+        
+        for(int k=i;j<k;j++)
+        {
+            if(cl[find[j][0]-1][find[j][1]]=='C')
+            {
+                cl[find[j][0]-1][find[j][1]]='F';
+                find[i][0]=find[j][0]-1;
+                find[i][1]=find[j][1];
+                i++;
+            }
+            if(cl[find[j][0]+1][find[j][1]]=='C')
+            {
+                cl[find[j][0]+1][find[j][1]]='F';
+                find[i][0]=find[j][0]+1;
+                find[i][1]=find[j][1];
+                i++;
+            }
+            if(cl[find[j][0]][find[j][1]-1]=='C')
+            {
+                cl[find[j][0]][find[j][1]-1]='F';
+                find[i][0]=find[j][0];
+                find[i][1]=find[j][1]-1;
+                i++;
+            }
+            if(cl[find[j][0]][find[j][1]+1]=='C')
+            {
+                cl[find[j][0]][find[j][1]+1]='F';
+                find[i][0]=find[j][0];
+                find[i][1]=find[j][1]+1;
+                i++;
+            }
+        }
+    }
+    for(int x=0;x<r;x++)
+    {
+        for(int y=0;y<c+1;y++)
+        {
+            printf("%c",cl[x][y]);
+        }
+        printf("\n");
+    }
+
+}
--- self learn/0-test.c
+++ self learn/0-test.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 
 int main(void) {
-    int a, b, ret;
-    a  = -1;
-    ret = scanf("%d%d", &a, &b);
-    printf("%d %d %d", ret, a, b);
+    char cl[3][3];
+    for(int x=0;x<3;x++)
+    {
+        for(int y=0;y<3;y++)
+        {
+            scanf("%c",&cl[x][y]);
+            
+        }
+    }
+    for(int x=0;x<3;x++)
+    {
+        for(int y=0;y<3;y++)
+        {
+            printf("%c",cl[x][y]);
+        }
+        printf("\n");
+    }
     return 0;
 }
 // test
