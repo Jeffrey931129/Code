6e4bdc6a158666d2dffea9a0da862383ac157b63
Jeffrey <168chen2011@gmail.com>
2023-11-21 00:08:58 +0800

    update

--- Fun/QB.c
+++ Fun/QB.c
@@ -3,19 +3,19 @@
 int main()
 {
     int time,sum=0;
-    printf("How many times do you want QB to run?");
+    printf("請問你想讓QB跑幾趟:");
     scanf("%d",&time);
     for(int Time=0;Time<time;Time++)
     {
-        printf("How far should QB run this time?");
+        printf("請輸入QB該跑多遠:");
         int far;
         scanf("%d",&far);
         sum+=far;
         if(sum>100)
         {
-            printf("QB is too tired.");
+            printf("QB累趴了");
             break;
         }
-        printf("QB has run %d m.\n",sum);
+        printf("QB已經跑了%dm\n",sum);
     }
 }
