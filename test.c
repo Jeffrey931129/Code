594ac54615ae595506da734aa6188bae2ec08cad
Jeffrey <168chen2011@gmail.com>
2023-11-23 09:31:09 +0800

    update

--- test.c
+++ test.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
+
 int main()
 {
     int num;
     scanf("%x",&num);  // 16進位
     printf("%x",num);
     return 0;
+}
+
+void hanoi(int n,int from,int to,int temp)
+{
+    if(n==1)
+    {
+        printf("Move disk 1 from %d to %d\n",from,to);
+    }
+    hanoi(n-1,from,temp,to);
+    printf("Move disk %d from %d to %d\n",n,from,to);
+    hanoi(n-1,temp,to,from);
 }
