09a03cd08e48dfe4d2f0aec42393ceb446ac115f
Jeffrey <168chen2011@gmail.com>
2023-11-23 21:39:05 +0800

    update

--- test.c
+++ test.c
@@ -1,21 +1,40 @@
 #include <stdio.h>
+#define MAX 10
+#define swap(a, b) if (a != b) {(*a)^=(*b); (*b)^=(*a); (*a)^=(*b);}
 
+void selection_sort(int* a, int size);
+int a[MAX] = {3, 7, 9, 1, 4, 0, 8, 6, 2, 5},get_min(int*,int);
 
-int main()
-{
-    int num;
-    scanf("%x",&num);  // 16進位
-    printf("%x",num);
+int main(void) {
+    selection_sort(a, MAX);
+    for (int i = 0; i < MAX; i++) {
+        printf("%d ", a[i]);
+    }
     return 0;
 }
 
-void hanoi(int n,int from,int to,int temp)
+void selection_sort(int* a,int size)
 {
-    if(n==1)
+    int min;
+    if(size==1)
     {
-        printf("Move disk 1 from %d to %d\n",from,to);
+        return;
     }
-    hanoi(n-1,from,temp,to);
-    printf("Move disk %d from %d to %d\n",n,from,to);
-    hanoi(n-1,temp,to,from);
+    min=get_min(a,size);
+    swap(&a[0],&a[min]);
+    selection_sort(a+1,size-1);
+}
+
+int get_min(int* a,int size)
+{
+    if(size==1)
+    {
+        return 0;
+    }
+    int min=get_min(a+1,size-1)+1;
+    if(a[min]>a[0])
+    {
+        return 0;
+    }
+    return min;
 }
