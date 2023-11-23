8ff7ea3184cfa5c7df451c26397eb627deb4e654
Jeffrey <168chen2011@gmail.com>
2023-11-23 21:49:06 +0800

    update

--- test.c
+++ test.c
@@ -1,40 +1,35 @@
 #include <stdio.h>
-#define MAX 10
-#define swap(a, b) if (a != b) {(*a)^=(*b); (*b)^=(*a); (*a)^=(*b);}
+#define MAX 10000
 
-void selection_sort(int* a, int size);
-int a[MAX] = {3, 7, 9, 1, 4, 0, 8, 6, 2, 5},get_min(int*,int);
+typedef struct 
+{
+    int n,from,to,temp;
+} Record;
+Record stack[MAX];
+int top=-1;
+
+void move_hanoi(int n,int from,int to,int temp) 
+{
+    stack[++top]=(Record){n,from,to,temp};
+    while(top!=-1) 
+    {
+        Record record=stack[top--];
+        if(record.n==1||record.temp==-1) 
+        {
+            printf("Move disk %d from %d to %d\n",record.n,record.from,record.to);
+            continue;
+        }
+        stack[++top]=(Record){record.n-1,record.temp,record.to,record.from};
+        stack[++top]=(Record){record.n,record.from,record.to,-1};
+        stack[++top]=(Record){record.n-1,record.from,record.temp,record.to};
+    }
+}
+
+
+
 
 int main(void) {
-    selection_sort(a, MAX);
-    for (int i = 0; i < MAX; i++) {
-        printf("%d ", a[i]);
-    }
+    move_hanoi(3, 1, 3, 2);
     return 0;
 }
 
-void selection_sort(int* a,int size)
-{
-    int min;
-    if(size==1)
-    {
-        return;
-    }
-    min=get_min(a,size);
-    swap(&a[0],&a[min]);
-    selection_sort(a+1,size-1);
-}
-
-int get_min(int* a,int size)
-{
-    if(size==1)
-    {
-        return 0;
-    }
-    int min=get_min(a+1,size-1)+1;
-    if(*(a+min)>*a)
-    {
-        return 0;
-    }
-    return min;
-}
