38d80a5f0a4e73dd915365f2b726d386e6d33cf8
Jeffrey <168chen2011@gmail.com>
2023-12-06 15:24:46 +0800

    update

--- class/LAB9/Text.c
+++ class/LAB9/Text.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
 #include <string.h>
-
 #define MAX_SIZE 1005
 
 char input[MAX_SIZE];
-
+char temp[MAX_SIZE];
 char command[MAX_SIZE];
 int j;
 
 int main()
-
 {
-
     scanf(" %[^\n]",input);
-
     scanf(" %[^\n]",command);
-
-    /* your code here */
     for(int i=0;command[i]!=0;i++)
     {
         if(command[i]=='b')
         {
             if(j>0) 
             {
-                strcpy(&input[j-1],&input[j]);
+                strcpy(temp,&input[j]);
+                strcpy(&input[j-1],temp);
                 j--;
             }
         }
@@ -36,6 +31,5 @@ int main()
         }
     }
     printf("%s", input);
-
     return 0;
 }
