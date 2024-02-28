b243ca77b905bb48311011b9f06ba6ac7fea5899
Jeffrey <168chen2011@gmail.com>
2024-02-28 16:09:41 +0800

    update

---
+++ class/112-2/HW1/Game.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n,k;
+        scanf("%d%d",&n,&k);
+        int s[n];
+        for(int i=0;i<n;i++) s[i]=i+1;
+        while(k--)
+        {
+            int a;
+            scanf("%d",&a);
+            if(s[a-1]>=n) printf("Penguin07 QQ\n");
+            else
+            {
+                printf("%d\n",s[a-1]+1);
+                s[s[a-1]]=n,s[a-1]++;
+            } 
+            
+        }
+    }
+}
