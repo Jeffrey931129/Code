d7344db809fd2bb242b34e637eb45dd1346cf173
Jeffrey <168chen2011@gmail.com>
2024-03-21 10:21:55 +0800

    update

--- class/112-2/HW2/Construct2.c
+++ class/112-2/HW2/Construct2.c
@@ -2,11 +2,15 @@
 #include <stdlib.h>
 
 void solve_preorder(int pos,int num,int pos_inorder);
-int inorder[200005],preorder[200005];
+int inorder[200005],preorder[200005],inorderpos[200005];
 int main()
 {
     int n; scanf("%d",&n);
-    for(int i=0;i<n;i++) scanf("%d",&inorder[i]);
+    for(int i=0;i<n;i++) 
+    {
+        scanf("%d",&inorder[i]);
+        inorderpos[inorder[i]]=i;
+    }
     for(int i=0;i<n;i++) scanf("%d",&preorder[i]);
     solve_preorder(0,n,0);
     puts("");
@@ -14,8 +18,7 @@ int main()
 
 void solve_preorder(int pos,int num,int pos_inorder)
 {
-    int left_num=0;
-    while(inorder[pos_inorder+left_num]!=preorder[pos]) left_num++;
+    int left_num=inorderpos[preorder[pos]]-pos_inorder;
     if(left_num>1) solve_preorder(pos+1,left_num,pos_inorder);
     else if(left_num==1) 
     {
