23c9b8f1d15d8ab42da75d2ac4d70e72225d3a64
Jeffrey <168chen2011@gmail.com>
2024-05-16 11:38:43 +0800

    update

--- fun/TaiwanUniversity/DSA/HW0/Problem3.c
+++ fun/TaiwanUniversity/DSA/HW0/Problem3.c
@@ -5,9 +5,10 @@
 typedef struct _Node{
     int x;
     struct _Node* next;
+    struct _Node* prev;
 }node;
 
-void connect(node* temp_a,node* temp_b);
+int connect(node* temp_a,node* temp_b);
 void print(node* arr,int num);
 
 int main(){
@@ -16,7 +17,7 @@ int main(){
     node** arr=malloc(sizeof(node*)*n);
     for(int i=0;i<n;i++){
         arr[i]=malloc(sizeof(node));
-        arr[i]->x=-1,arr[i]->next=NULL;
+        arr[i]->x=-1,arr[i]->next=NULL,arr[i]->prev=NULL;
     }
     for(int i=0;i<m;i++){
         int type; scanf("%d",&type);
@@ -27,12 +28,14 @@ int main(){
                 arr[stack-1]->next=malloc(sizeof(node));
                 arr[stack-1]->next->x=num;
                 arr[stack-1]->next->next=NULL;
+                arr[stack-1]->next->prev=arr[stack-1];
             }
             else{
                 node* temp=arr[stack-1]->next;
                 while(temp->next!=NULL) temp=temp->next;
                 temp->next=malloc(sizeof(node));
                 temp->next->x=num,temp->next->next=NULL;
+                temp->next->prev=temp;
             }
         }
         else if(type==2){
@@ -56,6 +59,7 @@ int main(){
                 while(temp->next!=NULL) temp=temp->next;
                 temp->next=malloc(sizeof(node));
                 temp->next->x=arr[a-1]->x,temp->next->next=arr[a-1]->next;
+                temp->next->prev=temp; arr[a-1]->next->prev=temp->next;
             }
             arr[a-1]->x=-1,arr[a-1]->next=NULL;
         }
@@ -66,11 +70,16 @@ int main(){
                 arr[b-1]->x=arr[a-1]->x,arr[b-1]->next=arr[a-1]->next;
                 arr[a-1]->x=-1,arr[a-1]->next=NULL; continue;
             }
-            node *temp_a=arr[a-1]->next,*temp_b=arr[b-1];
-            connect(temp_a,temp_b);
-            node* temp=malloc(sizeof(node));
-            temp->x=arr[a-1]->x,temp->next=arr[b-1];
-            arr[b-1]=temp; arr[a-1]->x=-1,arr[a-1]->next=NULL;
+            node *temp_a=arr[a-1],*temp_b=arr[b-1];
+            if(arr[a-1]->next!=NULL){
+                while(temp_a->next->next!=NULL) temp_a=temp_a->next;
+                while(temp_b->next!=NULL) temp_b=temp_b->next;
+                if(connect(temp_a,temp_b)){
+                    arr[b-1]=arr[a-1];
+                }
+            }
+            arr[a-1]=malloc(sizeof(node));
+            arr[a-1]->x=-1,arr[a-1]->next=NULL;
         }
     }
     for(int i=0;i<n;i++){
@@ -79,14 +88,21 @@ int main(){
     }
 }
 
-void connect(node* temp_a,node* temp_b){
-    if(temp_a->next!=NULL&&temp_b->next!=NULL) connect(temp_a->next,temp_b->next);
-    if(temp_b->next==NULL){
-        temp_b->next=temp_a;
+int connect(node* temp_a,node* temp_b){
+    temp_b->next=temp_a->next; temp_a->next=temp_b;
+    temp_b->next->prev=temp_b; 
+    if(temp_a->prev!=NULL&&temp_b->prev!=NULL){
+        return connect(temp_a->prev,temp_b->prev);
+        temp_b->prev=temp_a;
     }
-    else{
-        temp_a->next=temp_b->next;
-        temp_b->next=temp_a;
+    else if(temp_a->prev==NULL){
+        temp_b->prev->next=temp_a; 
+        temp_a->prev=temp_b->prev; temp_b->prev=temp_a;
+        return 0;
+    }
+    else if(temp_b->prev==NULL){
+        temp_b->prev=temp_a;
+        return 1;
     }
 }
 
