4a863e567b672ac200e7be2f582919928e58a1ae
Jeffrey <168chen2011@gmail.com>
2024-07-03 20:28:40 +0800

    update

---
+++ python/game/script.py
@@ -0,0 +1,131 @@
+import pyautogui
+import time
+
+# 等待2秒，讓你有時間切換到你希望輸入的窗口
+time.sleep(2)
+X, Y = pyautogui.size()
+
+def Click(x, y) :
+    pyautogui.moveTo(x, y, duration=0.3)
+    pyautogui.click()
+    print(f"點擊 ({x}, {y})")
+
+def Press(c) :
+    pyautogui.press(c)
+    print(f"按下 {c}")
+
+def Move(c, duration) :
+    pyautogui.keyDown(c)  # 按下鍵
+    print(f"按住 {c}")
+    time.sleep(duration)  # 等待指定時間
+    pyautogui.keyUp(c)  # 釋放鍵
+
+def Check_Drop() :
+    screenshot = pyautogui.screenshot()
+    drop = screenshot.getpixel((1250, 580))
+    if drop[0] == 224 and drop[1] == 224 and drop[2] == 224 :
+        Press('E')
+        print("獲得聲骸")
+        time.sleep(1)
+        return True
+    for i in range(1600) :
+        drop = screenshot.getpixel((i + 160, 440))
+        if 220 <= drop[0] and 205 <= drop[1] <= 220 and 180 <= drop[2] <= 200 :
+            print(f"聲骸掉落 ({i + 160}, 260)")
+            return True
+    print("沒有聲骸掉落")
+    return False
+
+def Go_To_Drop() :
+    while True :
+        screenshot = pyautogui.screenshot()
+        drop = screenshot.getpixel((1250, 580))
+        if drop[0] == 224 and drop[1] == 224 and drop[2] == 224 :
+            Press('E')
+            print("獲得聲骸")
+            time.sleep(1)
+            return
+        Move('W', 0.5)
+        for i in range(1600) :
+            drop = screenshot.getpixel((i + 160, 440))
+            if 220 <= drop[0] and 205 <= drop[1] and 180 <= drop[2] <= 200 :
+                if i <= 800 :
+                    Move('A', 0.5)
+                else :
+                    Move('D', 0.5)
+                break
+
+# 正式開始
+Press('E')
+time.sleep(1)
+Click(430, 310)
+Click(1460, 950)
+Click(1180, 690)
+time.sleep(0.5)
+Click(1540, 1000)
+
+# 檢測進入副本
+while True :
+    time.sleep(0.1)
+    screenshot = pyautogui.screenshot()
+    dragon_blood = screenshot.getpixel((967, 102))
+    print(f"顏色 ({dragon_blood})")
+    if dragon_blood[0] == 255 and dragon_blood[1] == 255 and dragon_blood[2] == 255 :
+        print("戰鬥開始")
+        battle_time = time.time()
+        dragon_blood_time = time.time()
+        break
+
+# 戰鬥
+Move('W', 2)
+while True :
+    screenshot = pyautogui.screenshot()
+    dragon_blood = screenshot.getpixel((967, 102))
+    jinshi_Q = screenshot.getpixel((1492, 1007))
+    jinshi_F = screenshot.getpixel((1668, 942))
+    print(f"顏色 ({dragon_blood})")
+    if time.time() - battle_time >= 4 :
+        Click(960, 570)
+    if dragon_blood[0] != 224 and dragon_blood[1] != 224 and dragon_blood[2] != 224 and \
+        time.time() - dragon_blood_time >= 5 :
+        print("角 已死亡")
+        time.sleep(2)
+        break
+    if jinshi_Q[0] == 224 and jinshi_Q[1] == 224 and jinshi_Q[2] == 190 :
+        Press('R')
+        if jinshi_F[0] == 213 and jinshi_F[1] == 199 and jinshi_F[2] == 106 :
+            Press('F')
+            time.sleep(3)
+        Press('Q')
+        print("自動施放強化戰技")
+        dragon_blood_time = time.time() - 1
+    time.sleep(0.05)
+
+# 聲骸
+count = 4
+while count :
+    Press('H')
+    Press('H')
+    time.sleep(1)
+    if Check_Drop() :
+        Go_To_Drop()
+        break
+    Move('D', 0.5)
+    count -= 1
+
+# # 離開
+# Press('esc')
+# Click(1240, 690)
+# while True :
+#     time.sleep(1)
+#     screenshot = pyautogui.screenshot()
+#     lobby = screenshot.getpixel((1610, 110))
+#     if lobby[0] == 224 and lobby[1] == 224 and lobby[2] == 224 :
+#         print("返回主頁")
+#         break
+# pyautogui.rightClick()
+
+
+
+
+
--- test.cpp
+++ test.cpp
@@ -1,29 +1,7 @@
-#include <Windows.h>
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    int X = GetSystemMetrics(SM_CXSCREEN);
-    int Y = GetSystemMetrics(SM_CYSCREEN);
-    POINT cursorPos;
-    
-    Sleep(20); // 延遲 100 毫秒
-    if (GetCursorPos(&cursorPos)) {
-        cout << "Current cursor position: (" << cursorPos.x << ", " << cursorPos.y << ")" << endl;
-    } else {
-        cerr << "Failed to get cursor position." << endl;
-    }
-    // 模擬按下 Q 鍵
-    // keybd_event(0x51, 0, 0, 0); // 0x51 是 Q 鍵的虛擬鍵碼
-    // 模擬左鍵按下
-    SetCursorPos(1154, 669);
-    mouse_event(MOUSEEVENTF_LEFTDOWN, 1154, 669, 0, 0);
-    // cout << X << " " << Y << endl;
-    // 模擬釋放 Q 鍵
-    // keybd_event(0x51, 0, KEYEVENTF_KEYUP, 0);
-    mouse_event(MOUSEEVENTF_LEFTUP, 1154, 669, 0, 0);
-
-    cout << "Q key pressed and released." << endl;
-
     return 0;
 }
