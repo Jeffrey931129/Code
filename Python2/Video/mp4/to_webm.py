2145f086e87caea1f97a23dc790360fed41c48dc
Jeffrey <168chen2011@gmail.com>
2024-07-11 09:48:21 +0800

    update

--- Python2/Video/mp4/to_webm.py
+++
@@ -1,9 +0,0 @@
-from moviepy.editor import VideoFileClip
-
-# 讀取 MP4 視頻
-input_path = "C:/Users/USER/Downloads/jinshi.mp4"
-video = VideoFileClip(input_path)
-
-# 輸出 WebM 視頻
-output_path = "C:/Users/USER/Downloads/4-test.webm"
-video.write_videofile(output_path, codec='libvpx', audio_codec='libvorbis', bitrate='10000k')
